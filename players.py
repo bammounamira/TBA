0770e181a457c0f2dacbc53c41c5e3872b59f092
bammounamira <amira.bammoun@edu.esiee.fr>
2024-11-13 16:10:19 +0000

    add player class

---
+++ players.py
@@ -0,0 +1,17 @@
+class Player(): 
+    #Define the constructor
+    def __init__(self, name, hair, eyes, height, style):
+        self.name = name
+        self.hair = hair
+        self.eyes = eyes
+        self.height = height
+        self.style = style
+        
+
+
+
+
+
+
+
+
