3b37dfe91401e88186916cb75fbd9e6ed4a2976c
bammounamira <amira.bammoun@edu.esiee.fr>
2024-11-27 22:16:46 +0100

    Update Item.py

--- Item.py
+++ Item.py
@@ -4,5 +4,4 @@ class Item():
     def __init__(self, name, description, price):
         self.name= name
         self.description= description
-        self.price= price
-    
+        self.price= int(price)
