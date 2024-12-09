fd54d2a721fb33ccd60855dd7d8ae4c60c0de16c
Myriam-code741 <myriam.diez@edu.esiee.fr>
2024-12-09 16:51:40 +0000

    ajout PNJ

--- character.py
+++ character.py
@@ -6,21 +6,3 @@ class Character():
         self.description= description 
         self.current_room= current_room
         self.dialogues= dialogues
-    
-    def parler( self):
-        for i, dialogue in enumerate(self.dialogues):
-            print(f"{i+1}: {dialogue}")
-
-    def interagir (self):
-        print(f"{self.nom} dit : 'self.dialogues[0]'")
-
-    pnjs = {
-    "hall_entry": [
-        PNJ("saleswoman", "a woman who is dressed with elegance"
-            ["Welcome, Shopper.", "You can have acces to many articles."]),
-       
-    "": [
-        PNJ("Fée des bois", "Une petite fée lumineuse.", 
-            ["Prenez garde aux dangers dans la forêt.", "Les arbres parlent ici, écoutez bien."])
-    ]
-}
